a1988bc39f7610b1e274e24a568a0f21b3372780
pejman hkh <pejman.hkh@gmail.com>
2025-08-07 15:55:56 +0330

    Add height to player

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import 'react-player-cc/style.css'
 import ReactPlayerCC from 'react-player-cc'
+import './index.css'
 
 function App() {
   return <ReactPlayerCC src="./video.mp4" subtitles={[{ id: 1, title: 'test', link: "./sub.srt", default: true }]} />

6d98300692bdd636f6bc652cf37560f53f2b1466
pejman hkh <pejman.hkh@gmail.com>
2025-08-08 11:31:46 +0330

    Add HLS example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
 import 'react-player-cc/style.css'
-import ReactPlayerCC from 'react-player-cc'
+import ReactPlayerCC, { type PlayerInterface } from 'react-player-cc'
 import './index.css'
 
+import Hls from "hls.js";
+
+export class HlsVideoPlayer implements PlayerInterface {
+  private hls: Hls;
+  private video: HTMLVideoElement;
+
+  constructor(video: HTMLVideoElement) {
+    this.video = video;
+    this.hls = new Hls();
+    this.hls.attachMedia(video);
+  }
+
+  load(src: string) {
+    if (Hls.isSupported()) {
+      this.hls.loadSource(src);
+    } else if (this.video.canPlayType("application/vnd.apple.mpegurl")) {
+      this.video.src = src;
+    }
+  }
+
+  play() {
+    this.video.play();
+  }
+
+  pause() {
+    this.video.pause();
+  }
+
+  destroy() {
+    this.hls.destroy();
+  }
+}
+
 function App() {
-  return <ReactPlayerCC src="./video.mp4" subtitles={[{ id: 1, title: 'test', link: "./sub.srt", default: true }]} />
+  return <>
+    <h1>MP4 Example</h1>
+    <ReactPlayerCC src="./video.mp4" subtitles={[{ id: 1, title: 'test', link: "./sub.srt", default: true }]} />
+
+    <h1>HLS Example</h1>
+    <ReactPlayerCC playerClass={HlsVideoPlayer} src="./hls.m3u8" subtitles={[{ id: 1, title: 'test', link: "./sub.srt", default: true }]} />
+  </>
 }
 
 export default App;

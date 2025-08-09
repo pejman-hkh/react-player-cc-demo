b0b8f84c6067a051e90345254ed199d85a47233e
pejman hkh <pejman.hkh@gmail.com>
2025-08-09 20:47:11 +0330

    Add DASHJS example

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ReactPlayerCC, { type PlayerInterface } from 'react-player-cc'
 import './index.css'
 
 import Hls from "hls.js";
+import * as dashjs from 'dashjs';
 
 export class HlsVideoPlayer implements PlayerInterface {
   private hls: Hls;
@@ -35,12 +36,39 @@ export class HlsVideoPlayer implements PlayerInterface {
   }
 }
 
+export class DashVideoPlayer implements PlayerInterface {
+  private dash: dashjs.MediaPlayerClass;
+  private video: HTMLVideoElement;
+
+  constructor(video: HTMLVideoElement) {
+    this.video = video;
+    this.dash = dashjs.MediaPlayer().create();
+    this.dash.initialize(video, "", false);
+  }
+
+  load(src: string) {
+    this.dash.attachSource(src);
+  }
+
+  play() {
+    this.video.play();
+  }
+
+  pause() {
+    this.video.pause();
+  }
+
+  destroy() {
+    this.dash.reset();
+  }
+}
+
 function App() {
   return <>
 
     <div className="min-h-screen bg-gray-100 flex flex-col items-center p-2">
       {/* Header */}
-      
+
       <h1 className="text-3xl font-bold flex items-center"> <img src="logo.svg" width={100} /> React Player CC Demo</h1>
 
       {/* Player Card */}
@@ -52,6 +80,9 @@ function App() {
 
         <h2 className="text-2xl font-bold mt-8">HLS Example</h2>
         <ReactPlayerCC playerClass={HlsVideoPlayer} src="./hls.m3u8" subtitles={[{ id: 1, title: 'test', link: "./sub.srt", default: true }]} />
+
+        <h2 className="text-2xl font-bold mt-8">DASH Example</h2>
+        <ReactPlayerCC playerClass={DashVideoPlayer} src="https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd" subtitles={[{ id: 1, title: 'test', link: "./sub.srt", default: true }]} />
 
       </div>
     </div>
